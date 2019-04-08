8c12baeda776474c01b415c536416fdf775b6cf3
Dieter Hubau <dhubau@gmail.com>
2019-04-08 16:58:57 +0200

    Added node-sass fix

--- ci/build.sh
+++ ci/build.sh
@@ -4,6 +4,7 @@ mv cache/node_modules source
 
 cd source
 
+npm rebuild node-sass # temporary fix
 npm run build
 
 cp -R dist ../artifact/
--- ci/test.sh
+++ ci/test.sh
@@ -4,4 +4,5 @@ mv cache/node_modules source
 
 cd source
 
+npm rebuild node-sass # temporary fix
 npm run test
